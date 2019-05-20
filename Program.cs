3e866504cb2e4e2d88fd65887fde98a527c40af6
Tessa Biava <TBiava@my.gcu.edu>
2019-05-19 22:51:44 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// use for IC08
+//Lydia's Code
+//Corrected by Tessa
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CST_117_Exercise_9
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //make some sets
+            Set A = new Set();
+            Set B = new Set();
+
+            //put some stuff in the sets
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                //Do to checking against duplicates in Set, there will never be more than 4 elements, but we are looping through this 10 times.
+                //A.addElement(r.Next(4));
+                //Updated to 10 to get a max of ten elements.
+                A.addElement(r.Next(10));
+                B.addElement(r.Next(12));
+            }
+
+            //display each set and the union
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            Console.WriteLine("A union B: " + A.union(B));
+
+            //display original sets (should be unchanged)
+            Console.WriteLine("After union operation");
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Set.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Lydia's code - find the errors!
+//Corrected by Tessa
+
+namespace CST_117_Exercise_9
+{
+    class Set
+    {
+        private List<int> elements;
+
+
+        public Set()
+        {
+            elements = new List<int>();
+        }
+
+        public bool addElement(int val)
+        {
+            if (ContainsElement(val)) return false;
+            else
+            {
+                elements.Add(val);
+                return true;
+            }
+        }
+
+        private bool ContainsElement(int val)
+        {
+            return elements.Contains(val);
+
+            //replaced with Built in List<T>.Contains method
+            //for (int i = 0; i < elements.Count; i++)
+            //{
+            //    if (val == elements[i])
+            //        return true;
+            //    else
+            //        return false;
+            //}
+            //return false;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (int i in elements)
+            {
+                str += i + " ";
+            }
+            return str;
+        }
+
+        public void clearSet()
+        {
+            elements.Clear();
+        }
+
+        public Set union(Set rhs)
+        {
+            //Fix: needs to return a new Set object instead of adding onto current object.
+            Set union = new Set();
+
+            //Add the current elements to the new object.
+            for (int i = 0; i < this.elements.Count; i++)
+            {
+                union.addElement(this.elements[i]);
+            }
+            for (int i = 0; i < rhs.elements.Count; i++)
+            {
+                //Adds elements to the current object
+                //this.addElement(rhs.elements[i]);
+                union.addElement(rhs.elements[i]);
+            }
+            return union;
+        }
+    }
+}
